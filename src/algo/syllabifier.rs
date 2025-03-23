2c0e93b319a54ff7f976041097387a19e5770a9b
Monine Melolaris <alpzmj9@icloud.com>
2025-03-23 14:23:00 +0800

    Update syllabifier.rs.

--- src/algo/syllabifier.rs
+++ src/algo/syllabifier.rs
@@ -2,10 +2,10 @@ use std::{
     cell::RefCell,
     cmp::Reverse,
     collections::{BTreeMap, BinaryHeap},
-    sync::Arc,
+    rc::Rc,
 };
 
-use crate::dict::corrector::Corrector;
+use crate::dict::{corrector::Corrector, prism::Prism};
 
 use super::spelling::{SpellingProperties, SpellingType};
 
@@ -36,11 +36,11 @@ impl EdgeProperties {
 }
 
 type SpellingMap = BTreeMap<SyllableId, EdgeProperties>;
-type VertexMap = BTreeMap<usize, SpellingType>;
+type VertexMap = RefCell<BTreeMap<usize, SpellingType>>;
 type EndVertexMap = BTreeMap<usize, SpellingMap>;
 type EdgeMap = BTreeMap<usize, EndVertexMap>;
 
-type SpellingPropertiesList = Vec<Arc<EdgeProperties>>;
+type SpellingPropertiesList = Vec<Rc<EdgeProperties>>;
 type SpellingIndex = BTreeMap<SyllableId, SpellingPropertiesList>;
 type SpellingIndices = BTreeMap<usize, SpellingIndex>;
 
@@ -57,7 +57,7 @@ impl Default for SyllableGraph {
         Self {
             input_lenth: 0,
             interpreted_length: 0,
-            vertices: VertexMap::new(),
+            vertices: RefCell::new(BTreeMap::new()),
             edges: EdgeMap::new(),
             indices: SpellingIndices::new(),
         }
@@ -106,80 +106,54 @@ impl Syllabifier {
     }
 
     // TODO
-    /*  pub fn build_syllable_graph(
-            input: String,
-            prism: &mut Prism,
-            graph: &mut SyllableGraph,
-        ) -> i32 {
-            if input.is_empty() {
-                return 0;
-            };
+    pub fn build_syllable_graph(
+        input: String,
+        prism: &mut Prism,
+        graph: &mut SyllableGraph,
+    ) -> i32 {
+        if input.is_empty() {
+            return 0;
+        };
 
-            let mut farthest: usize = 0;
-            let mut queue: VertexQueue = BinaryHeap::new();
-            queue.push(Reverse((0, SpellingType::NormalSpelling))); // start
+        let mut farthest: usize = 0;
+        let mut queue: VertexQueue = BinaryHeap::new();
+        queue.push(Reverse((0, SpellingType::NormalSpelling))); // start
 
-            while let Some(Reverse(vertex)) = queue.pop() {
-                let current_pos = vertex.0;
+        while let Some(Reverse(vertex)) = queue.pop() {
+            let current_pos = vertex.0;
 
-                // record a visit to the vertex
-                if !graph.vertices.contains_key(&current_pos) {
-                    graph.vertices.insert(current_pos, vertex.1); // preferred spelling type comes first
-                } else {
-                    // *graph.vertices.get_mut(&current_pos).unwrap() =
-                    //     std::cmp::min(vertex.1, *graph.vertices.get(&current_pos).unwrap());
-                    continue; // discard worse spelling types
-                }
-                if current_pos > farthest {
-                    farthest = current_pos;
-                    // 日志相关无法直接迁移 DLOG(INFO) << "current_pos: " << current_pos;
-
-                    // see where we can go by advancing a syllable
-                    todo!("依赖 dict/prism {}", farthest);
-                }
+            // record a visit to the vertex
+            if !graph.vertices.borrow().contains_key(&current_pos) {
+                graph.vertices.borrow_mut().insert(current_pos, vertex.1); // preferred spelling type comes first
+            } else {
+                // *graph.vertices.get_mut(&current_pos).unwrap() =
+                //     std::cmp::min(vertex.1, *graph.vertices.get(&current_pos).unwrap());
+                continue; // discard worse spelling types
             }
+            if current_pos > farthest {
+                farthest = current_pos;
+                // 日志相关无法直接迁移 DLOG(INFO) << "current_pos: " << current_pos;
 
-            todo!(
-                "build_syllable_graph() 依赖其他代码，尚未实现{:?}， {}，{} ",
-                prism,
-                COMPLETION_PENALTY,
-                CORRECTION_CREDIBILITY
-            );
-        }
-
-        pub fn enable_correction(&mut self, corrector: Option<Box<Corrector>>) {
-            self.corrector = corrector;
-        }
-    */
-    /*pub(crate) fn check_overlapped_spellings0(
-        &self,
-        mut graph: Option<SyllableGraph>,
-        start: usize,
-        end: usize,
-    ) {
-        if let Some(mut graph) = graph {
-            // 遍历 indices 中的每个顶点
-            for (vertex_id, spelling_index) in &mut graph.indices {
-                // 遍历每个 SyllableId 对应的 SpellingPropertiesList
-                for (syllable_id, spelling_properties_list) in spelling_index {
-                    // 遍历每个 Arc<EdgeProperties>
-                    for edge_properties_arc in spelling_properties_list {
-                        // 获取 Arc 的可变引用
-                        if let Some(edge_properties) = Arc::get_mut(edge_properties_arc) {
-                            // 检查 end_pos 是否在指定的范围内
-
-                            // 修改 credibility 的值
-                            edge_properties.spelling_properties.credibility = 1.0;
-                        }
-                    }
-                }
+                // see where we can go by advancing a syllable
+                todo!("依赖 dict/prism {}", farthest);
             }
         }
-    }*/
+
+        todo!(
+            "build_syllable_graph() 依赖其他代码，尚未实现{:?}， {}，{} ",
+            prism,
+            COMPLETION_PENALTY,
+            CORRECTION_CREDIBILITY
+        );
+    }
+
+    pub fn enable_correction(&mut self, corrector: Option<Box<Corrector>>) {
+        self.corrector = corrector;
+    }
 
     pub(crate) fn check_overlapped_spellings(
         &self,
-        graph: Option<SyllableGraph>,
+        graph: Option<&mut SyllableGraph>,
         start: usize,
         end: usize,
     ) {
@@ -210,13 +184,15 @@ impl Syllabifier {
                         // discourage syllables at an ambiguous joint
                         // bad cases include pinyin syllabification "niju'ede"
                         for spelling in x_value.values() {
-                            spelling
-                                .spelling_properties
-                                .try_borrow_mut()
-                                .unwrap()
-                                .credibility += PENALTY_FOR_AMBIGUOUS_SYLLABLE;
+                            spelling.spelling_properties.borrow_mut().credibility +=
+                                PENALTY_FOR_AMBIGUOUS_SYLLABLE;
                         }
+                        graph
+                            .vertices
+                            .borrow_mut()
+                            .insert(*joint, SpellingType::AmbiguousSpelling);
                     }
+                    break;
                 }
             }
         }
@@ -233,7 +209,7 @@ impl Syllabifier {
                     index
                         .entry(*spelling_first as SyllableId)
                         .or_insert_with(Vec::new)
-                        .push(Arc::new(spelling_second.clone()));
+                        .push(Rc::new(spelling_second.clone()));
                 }
             }
         }

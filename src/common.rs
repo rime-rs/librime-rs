a79ab213b80715762734e000452c87fbb975b0f7
Monine Melolaris <alpzmj9@icloud.com>
2025-03-25 20:01:47 +0800

    Merge branch 'dev'
    # Conflicts:
    #	src/candidate.rs

---
+++ src/common.rs
@@ -0,0 +1,3 @@
+use std::rc::Rc;
+
+pub type An<T> = Rc<T>;
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 pub mod algo;
 pub mod candidate;
+pub mod config;
 pub mod dict;
 pub mod gear;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+mod common;
 
 #[cfg(test)]
 mod tests;
